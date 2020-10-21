4a519eb477a39b558a5312159cb853285c65d69e
Paul Schmutz <paul.s@pandocs.com>
2020-10-21 09:19:29 +0200

    Set default version to 13 for System.getProperty "java.version" and "javafx.version"

--- src/main/java/at/fhhagenberg/sqe/SystemInfo.java
+++ src/main/java/at/fhhagenberg/sqe/SystemInfo.java
@@ -3,11 +3,11 @@ package at.fhhagenberg.sqe;
 public class SystemInfo {
 
     public static String javaVersion() {
-        return System.getProperty("java.version");
+        return System.getProperty("java.version", "13");
     }
 
     public static String javafxVersion() {
-        return System.getProperty("javafx.version");
+        return System.getProperty("javafx.version", "13");
     }
 
 }
