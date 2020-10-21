1fb8c3dd0f61ade75e06e2f3a3a7b6f3264db381
Harald Pinsker <S1910455003@students.fh-hagenberg.at>
2020-10-21 14:08:30 +0200

    implemented building floor service test

--- src/test/java/at/fhhagenberg/sqe/api/BuildingFloorServiceTest.java
+++ src/test/java/at/fhhagenberg/sqe/api/BuildingFloorServiceTest.java
@@ -1,15 +1,45 @@
 package at.fhhagenberg.sqe.api;
 
+import at.fhhagenberg.sqe.di.DI;
+import at.fhhagenberg.sqe.entity.BuildingFloor;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.rmi.RemoteException;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 public class BuildingFloorServiceTest {
 
+    private BuildingFloorService buildingFloorService;
+
+    @BeforeEach
+    public void setUp() {
+        buildingFloorService = DI.get(BuildingFloorService.class);
+    }
+
     @Test
     public void testGetAll() {
+        int totalNumberOfFloors = 10;
+
+        try {
+            List<BuildingFloor> floors = buildingFloorService.getAll(totalNumberOfFloors);
+            assertEquals(totalNumberOfFloors, floors.size());
+        } catch (RemoteException e) {
+            e.printStackTrace();
+        }
     }
 
     @Test
     public void testGet() {
+        try {
+            int floorNumber = 3;
+            BuildingFloor floor = buildingFloorService.get(floorNumber);
+            assertNotNull(floor);
+            assertEquals(floor.getFloorNumber(), floorNumber);
+        } catch (RemoteException e) {
+            e.printStackTrace();
+        }
     }
 }
