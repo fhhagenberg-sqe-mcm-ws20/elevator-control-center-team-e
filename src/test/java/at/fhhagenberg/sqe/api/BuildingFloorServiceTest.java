55d8fcc868aaebfb7b6a0102ebcd1c258ddae3ea
Paul Schmutz <paul.s@pandocs.com>
2020-10-21 17:58:23 +0200

    Adapt BuildingFloorServiceTest

--- src/test/java/at/fhhagenberg/sqe/api/BuildingFloorServiceTest.java
+++ src/test/java/at/fhhagenberg/sqe/api/BuildingFloorServiceTest.java
@@ -3,6 +3,7 @@ package at.fhhagenberg.sqe.api;
 import at.fhhagenberg.sqe.di.DI;
 import at.fhhagenberg.sqe.di.ElevatorControlSystemProvider;
 import at.fhhagenberg.sqe.entity.BuildingFloor;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
@@ -16,7 +17,7 @@ public class BuildingFloorServiceTest {
 
     @BeforeEach
     public void setUp() {
-        buildingFloorService = DI.get(BuildingFloorServiceImpl.class);
+        buildingFloorService = DI.get(BuildingFloorService.class);
     }
 
     @Test
@@ -24,9 +25,9 @@ public class BuildingFloorServiceTest {
         List<BuildingFloor> floors = buildingFloorService.getAll();
         assertNotNull(floors);
         assertEquals(ElevatorControlSystemProvider.FLOORS, floors.size());
+        floors.forEach(Assertions::assertNotNull);
         int i = 0;
         for (BuildingFloor floor : floors) {
-            assertNotNull(floor);
             assertEquals(i, floor.getFloorNumber());
             i++;
         }
@@ -44,7 +45,7 @@ public class BuildingFloorServiceTest {
 
         @Test
         public void testGetInvalidFloorNumber() throws RemoteException {
-            int floorNumber = ElevatorControlSystemProvider.FLOORS + 1;
+            int floorNumber = -1;
             BuildingFloor floor = buildingFloorService.get(floorNumber);
             assertNull(floor);
         }
