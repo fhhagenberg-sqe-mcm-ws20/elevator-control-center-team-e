41c41979b7685a396c389c222204c7fc716dd466
Paul Schmutz <paul.s@pandocs.com>
2020-10-27 17:30:32 +0100

    Check clock tick before and after getting data

--- src/main/java/at/fhhagenberg/sqe/repository/ElevatorRepository.java
+++ src/main/java/at/fhhagenberg/sqe/repository/ElevatorRepository.java
@@ -7,6 +7,7 @@ import at.fhhagenberg.sqe.entity.Elevator;
 import at.fhhagenberg.sqe.entity.ElevatorControlSystem;
 import at.fhhagenberg.sqe.entity.ServicedFloor;
 import at.fhhagenberg.sqe.model.Resource;
+import at.fhhagenberg.sqe.rmi.IElevator;
 import com.google.inject.Inject;
 
 public class ElevatorRepository {
@@ -14,21 +15,31 @@ public class ElevatorRepository {
     private final ElevatorControlSystemService elevatorControlSystemService;
     private final ElevatorService elevatorService;
     private final ServicedFloorService servicedFloorService;
+    private final IElevator elevatorControl;
 
     @Inject
     public ElevatorRepository(
             ElevatorControlSystemService elevatorControlSystemService,
             ElevatorService elevatorService,
-            ServicedFloorService servicedFloorService
+            ServicedFloorService servicedFloorService,
+            IElevator elevatorControl
     ) {
         this.elevatorControlSystemService = elevatorControlSystemService;
         this.elevatorService = elevatorService;
         this.servicedFloorService = servicedFloorService;
+        this.elevatorControl = elevatorControl;
     }
 
     public Resource<ElevatorControlSystem> getElevatorControlSystem() {
         try {
-            ElevatorControlSystem data = elevatorControlSystemService.get();
+            long diff;
+            ElevatorControlSystem data;
+            do {
+                long clockTick1 = elevatorControl.getClockTick();
+                data = elevatorControlSystemService.get();
+                long clockTick2 = elevatorControl.getClockTick();
+                diff = clockTick2 - clockTick1;
+            } while(diff != 0L);
             return Resource.success(data);
         } catch (Exception exception) {
             return Resource.error(exception);
