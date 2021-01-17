8fa1f483465e1c738257ed4dde6b80aa1831dcc8
Paul Schmutz <paul.s@pandocs.com>
2021-01-17 22:27:38 +0100

    Implement first okay version of auto mode

--- src/main/kotlin/at/fhhagenberg/sqe/task/AutoModeTask.kt
+++ src/main/kotlin/at/fhhagenberg/sqe/task/AutoModeTask.kt
@@ -2,6 +2,8 @@ package at.fhhagenberg.sqe.task
 
 import at.fhhagenberg.sqe.adapter.ElevatorAdapter
 import at.fhhagenberg.sqe.di.key.AutoModeProperty
+import at.fhhagenberg.sqe.entity.DoorState
+import at.fhhagenberg.sqe.entity.Elevator
 import at.fhhagenberg.sqe.entity.ElevatorControlSystem
 import at.fhhagenberg.sqe.store.ElevatorStore
 import com.google.inject.Inject
@@ -22,6 +24,27 @@ class AutoModeTask @Inject constructor(
     }
 
     private fun executeAutoModeLogic(elevatorControlSystem: ElevatorControlSystem) {
-        // TODO
+        val requestedFloors = elevatorControlSystem.floors
+            .filter { it.isDownActive || it.isUpActive }
+            .toMutableList()
+
+        for (elevator in elevatorControlSystem.elevators) {
+            val targetFloorButton = elevator.buttons.firstOrNull { it.isActive }
+            if (targetFloorButton != null) {
+                setTargetFloor(elevator, targetFloorButton.floorNumber)
+            } else {
+                requestedFloors.firstOrNull()?.let { nextRequestedFloor ->
+                    requestedFloors.remove(nextRequestedFloor)
+                    setTargetFloor(elevator, nextRequestedFloor.floorNumber)
+                }
+            }
+        }
+    }
+
+    private fun setTargetFloor(elevator: Elevator, targetFloor: Int) {
+        if (elevator.doorState == DoorState.CLOSED || elevator.doorState == DoorState.OPEN) {
+            elevator.targetFloor = targetFloor
+            elevatorAdapter.updateTargetFloor(elevator, elevator.targetFloor)
+        }
     }
 }
